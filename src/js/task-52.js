184b15bd7710f63ea880315c9c833aed3df191ca
Arif <arifkutq63@gmail.com>
2022-12-17 21:58:55 +0200

    reformat

--- src/js/task-52.js
+++ src/js/task-52.js
@@ -1,8 +1,24 @@
-function myFunction() {
-    const x = document.getElementById('myDiv');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
+window.addEventListener("DOMContentLoaded", () => {
+    let count = 0;
+    document.body.addEventListener("click", () => {
+        const length = 5;
+
+        for (let i = 0; i < length; i++) {
+            let x = document.createElement("article");
+
+            count += length;
+            x.setAttribute("id", "myArticle" + count);
+            document.body.appendChild(x);
+
+            let text = "some text..." + count;
+
+            document.getElementById("myArticle" + count).innerHTML = text;
+            document.getElementById("myArticle" + count)
+                .classList.add("message");
+        }
+    });
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        alert("boom");
+    });
+});
